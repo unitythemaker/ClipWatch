2b3aae658ad322c5772b4cf5227b5b523f71b7c5
UnityTheMaker <unity@themaker.cyou>
2022-05-20 15:05:09 +0300

    fix copying all selected texts not just clipboard

--- clipwatch.sh
+++ clipwatch.sh
@@ -8,7 +8,7 @@ test -f "$CW_PATH/history" || touch "$CW_PATH/history"
 while true;
 do
 	OLD=`cat $CW_PATH/old`
-	NEW=`xclip -o`
-	test "$OLD" = "$NEW" || (xclip -o | base64 >> $CW_PATH/history && xclip -o > $CW_PATH/old | base64 && echo $NEW)
+	NEW=`xclip -sel clip -o`
+	test "$OLD" = "$NEW" || (xclip -sel clip -o | base64 >> $CW_PATH/history && xclip -sel clip -o > $CW_PATH/old | base64 && echo $NEW)
 	sleep 1
 done
